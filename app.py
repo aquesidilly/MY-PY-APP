9de20eff70cae95b05246200102a0d2fd27c0176
aquesidilly <aquesidilly@yahoo.com>
2021-05-02 16:01:22 +0000

    MY-PY-APP

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import os
-from flask import Flask,flash, redirect, url_for,request, session
+from flask import Flask,flash, render_template, redirect, url_for,request, session
 from flask_pymongo import PyMongo 
 from bson.objectid import ObjectId
-if os.path.exists("envy.py"):
+if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
-# app.config['MONGO_URI'] = 'mongodb+srv://PY-aquesidilly:Nanaosei@cluster0.xmxi1.mongodb.net/Dillly?retryWrites=true&w=majority'
-app.config['mongodb+srv://PY-aquesidilly:Nanaosei@cluster0.xmxi1.mongodb.net/Dillly?retryWrites=true&w=majority'] = os.environ.get("mongodb+srv://PY-aquesidilly:Nanaosei@cluster0.xmxi1.mongodb.net/Dillly?retryWrites=true&w=majority")
+
+#app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
+app.config['MONGO_URI'] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
@@ -194,4 +195,4 @@ if __name__ == '__main__':
     app.config['DEBUG'] = True
     app.run(host='os.environ.get("IP")', 
     port=int(os.environ.get("PORT")),
-     debug=False)
+     debug=True)
